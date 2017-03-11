ee57da02f3796bdda909d36763ea692b42cce593
UnkleFungus <steven.anderson100692@gmail.com>
2017-03-11 04:19:58 -0700

    Revert "spells_added"

--- Pathfinder/Controler.js
+++ Pathfinder/Controler.js
@@ -6,10 +6,6 @@ function diceToggle() {
 function notesToggle() {
     $("#notesPage").toggle();
 }
-function spellsToggle(){
-    $("#spells").toggle();
-}
-
 function clear() {
 
 }
@@ -35,108 +31,51 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
-function addDiceHistory(die, number, roll) {
-    var sum = parseInt(number) + parseInt(roll);
-    if(roll == 20){
-        var row = "You rolled a D" + die + "(" + roll + ") plus " + number + " and got a " + sum + " CRITICAL!\n";
-    }
-    else if (roll == 1) {
-        var row = "You rolled a D" + die + "(" + roll + ") plus " + number + " and got a " + sum + " CRITICAL FAIL!\n";
-    }
-    else
-    var row = "You rolled a D" + die + "("+roll+") plus "+number+" and got a " + sum + "\n";
+function addDiceHistory(die,number) {
+    var row = "<tr>You rolled a D" + die + " and got a " + number + "</tr>";
     console.log(row);
-    $("#hisstory").val($("#hisstory").val()+row);
+    document.getElementById("diceHistoryTable").innerHTML += row;
 }
 
 function rollD2() {
     var roll = Math.floor((Math.random() * 2) + 1)
-    var add = parseInt($("#numPadText").val());
-    if(isNaN(add)){
-        add = 0;
-    }
-    confirm(roll + add);
-    addDiceHistory(2,add,roll);
+    confirm(roll);
+    addDiceHistory(2, roll);
 }
 
 function rollD4() {
     var roll = Math.floor((Math.random() * 4) + 1)
-    var add = parseInt($("#numPadText").val());
-    if (isNaN(add)) {
-        add = 0;
-    }
-    confirm(roll + add);
-    addDiceHistory(4,add,roll);
+    confirm(roll);
+    addDiceHistory(4, roll);
 }
 
 function rollD6() {
     var roll = Math.floor((Math.random() * 6) + 1)
-    var add = parseInt($("#numPadText").val());
-    if (isNaN(add)) {
-        add = 0;
-    }
-    confirm(roll + add);
-    addDiceHistory(6,add,roll);
+    confirm(roll);
+    addDiceHistory(6, roll);
 }
 
 function rollD8() {
     var roll = Math.floor((Math.random() * 8) + 1)
-    var add = parseInt($("#numPadText").val());
-    if (isNaN(add)) {
-        add = 0;
-    }
-    confirm(roll + add);
-    addDiceHistory(8,add,roll);
+    confirm(roll);
+    addDiceHistory(8, roll);
 }
 
 function rollD10() {
     var roll = Math.floor((Math.random() * 10) + 1)
-    var add = parseInt($("#numPadText").val());
-    if (isNaN(add)) {
-        add = 0;
-    }
-    confirm(roll + add);
-    addDiceHistory(10,add,roll);
+    confirm(roll);
+    addDiceHistory(10, roll);
 }
 
 function rollD12() {
     var roll = Math.floor((Math.random() * 12) + 1)
-    var add = parseInt($("#numPadText").val());
-    if (isNaN(add)) {
-        add = 0;
-    }
-    confirm(roll + add);
-    addDiceHistory(12,add,roll);
+    confirm(roll);
+    addDiceHistory(12, roll);
 }
 
 function rollD20() {
     var roll = Math.floor((Math.random() * 20) + 1)
-    var add = parseInt($("#numPadText").val());
-    if (isNaN(add)) {
-        add = 0;
-    }
-    if(roll == 20){
-        confirm("CRITICAL! " + (roll + add))
-    }
-    else if (roll == 1) {
-        confirm("CRITICAL FAIL! " + (roll + add))
-    }
-    else
-    confirm(roll + add);
-    addDiceHistory(20,add,roll);
+    confirm(roll);
+    addDiceHistory(20, roll);
 }
 
-function diceCalcType(number){
-    $("#numPadText").val($("#numPadText").val() + number);
-    clear0();
-}
-
-function backspace() {
-    $("#numPadText").val(Math.floor(parseInt($("#numPadText").val() / 10)));
-
-
-}
-
-function clear0() {
-    $("#numPadText").val(($("#numPadText").val() * 10) / 10);
-}
